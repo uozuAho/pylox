839c6f6d01c91e9ed6b296539539e5114006164b
Warwick Stone <uozu.aho@gmail.com>
2019-08-28 22:12:48 +1000

    test runner now accepts pattern

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python -m unittest discover -s tests -p '*_tests.py'
+TEST_PATTERN=$1
+if [ -z "$1" ]; then
+    TEST_PATTERN='*_tests.py'
+fi
+
+echo
+echo running tests matching $TEST_PATTERN
+echo
+
+python -m unittest discover -s tests -p "$TEST_PATTERN"
