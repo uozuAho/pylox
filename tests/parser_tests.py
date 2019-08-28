e73acee2854e7f122aaa93f2b151b617ac6da832
Warwick Stone <uozu.aho@gmail.com>
2019-08-28 22:12:59 +1000

    fix parser tests

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -10,48 +10,64 @@ EOF = Token(t.EOF, None, None, None)
 
 class ParserTests(unittest.TestCase):
 
-    @unittest.skip('todo: not sure if this is a valid statement')
     def test_single_string(self):
-        tokens = [Token(t.STRING, "asdf", "asdf", 1), EOF]
-        parser = Parser(tokens)
-        statements = list(parser.parse())
-
-        self.assertIsInstance(statements[0], Literal)
-
-    @unittest.skip('todo: not sure if this is a valid statement')
-    def test_unary_negative_number(self):
         tokens = [
-            Token(t.MINUS, "-", None, 1),
-            Token(t.NUMBER, "1", 1, 1),
+            Token(t.STRING, "asdf", "asdf", 1),
+            Token(t.SEMICOLON, ";", None, 1),
             EOF
         ]
         parser = Parser(tokens)
         statements = list(parser.parse())
 
-        self.assertIsInstance(statements, Unary)
-        self.assertEqual(statements.operator.type, t.MINUS)
-        self.assertIsInstance(statements.right, Literal)
-        self.assertEqual(statements.right.value, 1)
+        self.assertEqual(len(statements), 1)
+        statement = statements[0]
+        expression = statement.expression
 
-    @unittest.skip('todo: not sure if this is a valid statement')
+        self.assertIsInstance(expression, Literal)
+
+    def test_unary_negative_number(self):
+        tokens = [
+            Token(t.MINUS, "-", None, 1),
+            Token(t.NUMBER, "1", 1, 1),
+            Token(t.SEMICOLON, ";", None, 1),
+            EOF
+        ]
+        parser = Parser(tokens)
+        statements = list(parser.parse())
+
+        self.assertEqual(len(statements), 1)
+        statement = statements[0]
+        expression = statement.expression
+
+        self.assertIsInstance(expression, Unary)
+        self.assertEqual(expression.operator.type, t.MINUS)
+        self.assertIsInstance(expression.right, Literal)
+        self.assertEqual(expression.right.value, 1)
+
     def test_grouping_unary_negative_number(self):
         tokens = [
             Token(t.LEFT_PAREN, "(", None, 1),
             Token(t.MINUS, "-", None, 1),
             Token(t.NUMBER, "1", 1, 1),
             Token(t.RIGHT_PAREN, ")", None, 1),
+            Token(t.SEMICOLON, ";", None, 1),
             EOF
         ]
         parser = Parser(tokens)
         statements = list(parser.parse())
 
-        self.assertIsInstance(statements, Grouping)
-        self.assertIsInstance(statements.expression, Unary)
+        self.assertEqual(len(statements), 1)
+        statement = statements[0]
+        expression = statement.expression
+
+        self.assertIsInstance(expression, Grouping)
+        self.assertIsInstance(expression.expression, Unary)
 
     def test_print_string_literal(self):
         tokens = [
             Token(t.PRINT, "print", None, 1),
             Token(t.STRING, "yo", None, 1),
+            Token(t.SEMICOLON, ";", None, 1),
             EOF
         ]
         parser = Parser(tokens)
